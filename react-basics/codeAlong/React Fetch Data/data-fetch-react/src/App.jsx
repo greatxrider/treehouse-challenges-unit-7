57a7ff77827590731c5b7b78cde71f3c4987ff2d
greatxrider <daligdig.jephmari@gmail.com>
2024-08-19 23:43:54 +0800

    giphy app

--- react-basics/codeAlong/React Fetch Data/data-fetch-react/src/App.jsx
+++ react-basics/codeAlong/React Fetch Data/data-fetch-react/src/App.jsx
@@ -1,27 +1,52 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, act } from "react";
+import axios from 'axios';
 import SearchForm from './Components/SearchForm';
 import GifList from "./Components/GifList";
 
 function App() {
   const [gifs, setGifs] = useState([]);
+  const [query, setQuery] = useState("candy");
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    fetch('https://api.giphy.com/v1/gifs/trending?api_key=CfBMSPDz7FwGLC5c2WdvBnCv47CtdkSl&limit=25&offset=0&rating=g&bundle=messaging_non_clips')
-      .then(response => response.json())
-      .then(responseData => setGifs(responseData.data))
-      .catch(error => console.log('Error fetching and parsing data.', error));
-  }, []);
+    // fetch('https://api.giphy.com/v1/gifs/trending?api_key=CfBMSPDz7FwGLC5c2WdvBnCv47CtdkSl&limit=25&offset=0&rating=g&bundle=messaging_non_clips')
+    //   .then(response => response.json())
+    //   .then(responseData => setGifs(responseData.data))
+    //   .catch(error => console.log('Error fetching and parsing data.', error));
+    let activeFetch = true;
+    axios.get(`https://api.giphy.com/v1/gifs/search?q=${query}&api_key=CfBMSPDz7FwGLC5c2WdvBnCv47CtdkSl&limit=25&offset=0&rating=g&bundle=messaging_non_clips`)
+      .then(response => {
+        // handle success
+        if (activeFetch) {
+          setGifs(response.data.data);
+          setLoading(false);
+        }
+      })
+      .catch(error => {
+        // handle error
+        console.log("Error fetching and parsing data", error);
+      })
+    return () => { activeFetch = false }
+  }, [query]);
+
+  const handleQueryChange = searchText => {
+    setQuery(searchText);
+  }
 
   return (
     <div>
       <div className="main-header">
         <div className="inner">
           <h1 className="main-title">GifSearch</h1>
-          <SearchForm />
+          <SearchForm changeQuery={handleQueryChange} />
         </div>
       </div>
       <div className="main-content">
-        <GifList />
+        {
+          (loading)
+            ? <p>Loading...</p>
+            : <GifList data={gifs} />
+        }
       </div>
     </div>
   );
