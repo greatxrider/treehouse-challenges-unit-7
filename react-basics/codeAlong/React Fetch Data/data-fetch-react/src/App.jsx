67c53d5a95c5044a802e69507cb2c1d724ad1210
greatxrider <daligdig.jephmari@gmail.com>
2024-08-19 22:28:41 +0800

    update

---
+++ react-basics/codeAlong/React Fetch Data/data-fetch-react/src/App.jsx
@@ -0,0 +1,30 @@
+import React, { useState, useEffect } from "react";
+import SearchForm from './Components/SearchForm';
+import GifList from "./Components/GifList";
+
+function App() {
+  const [gifs, setGifs] = useState([]);
+
+  useEffect(() => {
+    fetch('https://api.giphy.com/v1/gifs/trending?api_key=CfBMSPDz7FwGLC5c2WdvBnCv47CtdkSl&limit=25&offset=0&rating=g&bundle=messaging_non_clips')
+      .then(response => response.json())
+      .then(responseData => setGifs(responseData.data))
+      .catch(error => console.log('Error fetching and parsing data.', error));
+  }, []);
+
+  return (
+    <div>
+      <div className="main-header">
+        <div className="inner">
+          <h1 className="main-title">GifSearch</h1>
+          <SearchForm />
+        </div>
+      </div>
+      <div className="main-content">
+        <GifList />
+      </div>
+    </div>
+  );
+};
+
+export default App;
