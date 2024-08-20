dec30c1f8ece19cd04f5ccbb31e40c7176e6fef2
greatxrider <daligdig.jephmari@gmail.com>
2024-08-20 15:53:46 +0800

    router passing data through routes

---
+++ react-basics/codeAlong/React Router/react-router/src/App.jsx
@@ -0,0 +1,36 @@
+// import React from "react";
+import { Route, Routes, Navigate } from "react-router-dom";
+import Header from "./components/Header";
+import About from "./components/About";
+import Courses from "./components/Courses";
+import Home from "./components/Home";
+import Teachers from "./components/Teachers";
+import CourseContainer from "./components/courses/CourseContainer";
+import { HTMLCourses } from "./data/courses";
+import { CSSCourses } from "./data/courses";
+import { JSCourses } from './data/courses';
+import NotFound from "./components/NotFound";
+
+function App() {
+  return (
+    <div className="container">
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="teachers" element={<Teachers />} />
+        <Route path="courses" element={<Courses />} >
+          <Route index element={<Navigate replace to="html" />} />
+          <Route path="html" element={<CourseContainer data={HTMLCourses} />} />
+          <Route path="css" element={<CourseContainer data={CSSCourses} />} />
+          <Route path="javascript" element={<CourseContainer data={JSCourses} />} />
+          <Route path="*" element={<Navigate replace to="/404" />} />
+        </Route>
+        <Route path="*" element={<Navigate replace to="/404" />} />
+        <Route path="404" element={<NotFound />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
---
+++ react-basics/codeAlong/React Router/react-router/src/components/Courses.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { NavLink, Outlet } from 'react-router-dom';
+import NotFound from './NotFound';
+
+const Courses = () => (
+  <div className="main-content courses">
+    <div className="course-header group">
+      <h2>Courses</h2>
+      <ul className="course-nav">
+        <li><NavLink to='html'>HTML</NavLink></li>
+        <li><NavLink to='css'>CSS</NavLink></li>
+        <li><NavLink to='javascript'>JavaScript</NavLink></li>
+      </ul>
+    </div>
+    <Outlet />
+  </div>
+);
+
+export default Courses;
